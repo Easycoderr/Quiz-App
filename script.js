c39a7554074b33d36a5c40631cad8193476b3cb5
EasyCoder <164244546+Easycoderr@users.noreply.github.com>
2025-03-24 17:40:55 +0300

    Fix bug & Add more Q

--- script.js
+++ script.js
@@ -27,6 +27,31 @@ let timer;
 
 const questions = [
   {
+  question: "Which planet is known as the Red Planet?",
+  options: ["Earth", "Mars", "Jupiter", "Saturn"],
+  correctAnswer: "Mars",
+},
+{
+  question: "What is the largest mammal in the world?",
+  options: ["Elephant", "Blue Whale", "Giraffe", "Rhino"],
+  correctAnswer: "Blue Whale",
+},
+{
+  question: "Who wrote the play 'Romeo and Juliet'?",
+  options: ["William Shakespeare", "Charles Dickens", "Mark Twain", "Jane Austen"],
+  correctAnswer: "William Shakespeare",
+},
+{
+  question: "What is the chemical symbol for water?",
+  options: ["O2", "H2O", "CO2", "NaCl"],
+  correctAnswer: "H2O",
+},
+{
+  question: "Which country hosted the 2020 Summer Olympics?",
+  options: ["China", "Brazil", "Japan", "USA"],
+  correctAnswer: "Japan",
+},
+  {
     question: "What is the capital of France?",
     options: ["Paris", "London", "Berlin", "Madrid"],
     correctAnswer: "Paris",
@@ -134,9 +159,9 @@ function cheackedAnswear() {
     setTimeout(() => nextQuestion(), 1000);
   } else {
     disableElements();
-    setTimeout(() => {
-      nextQuestion();
-    }, 1000);
+    // setTimeout(() => {
+    //   nextQuestion();
+    // }, 1000);
   }
 }
 function nextButton() {
